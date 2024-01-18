b52852066a68b433b8cb349a9b6423d4e2b93f8c
Minchae Kim <yjk199900@gmail.com>
2024-01-18 14:36:54 +0900

    Update EventManager.cs

--- EventManager.cs
+++ EventManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public enum EventType
 {
-    
+    // Add event type
 }
 
 public static class EventManager
@@ -27,12 +27,10 @@ public static class EventManager
     {
         if (eventTable.ContainsKey(eventName))
         {
-            // 해당 이벤트가 이미 존재하면 새로운 리스너를 리스트에 추가
             eventTable[eventName].Add(listener);
         }
         else
         {
-            // 해당 이벤트가 없으면 새로운 이벤트를 생성하고 첫 번째 리스너를 추가
             eventTable[eventName] = new List<Delegate> { listener };
         }
     }
@@ -51,10 +49,8 @@ public static class EventManager
     {
         if (eventTable.ContainsKey(eventName))
         {
-            // 해당 이벤트가 존재하면 리스너를 리스트에서 제거
             eventTable[eventName].Remove(listener);
 
-            // 모든 리스너가 제거되면 이벤트를 제거
             if (eventTable[eventName].Count == 0)
             {
                 eventTable.Remove(eventName);
@@ -71,17 +67,14 @@ public static class EventManager
     {
         if (eventTable.ContainsKey(eventName))
         {
-            // 해당 이벤트에 등록된 모든 리스너 호출
             foreach (var listener in eventTable[eventName])
             {
                 if (listener is Action actionListener)
                 {
-                    // 리스너가 Action 형태일 경우
                     actionListener?.Invoke();
                 }
                 else if (listener is Action<object> actionObjectListener)
                 {
-                    // 리스너가 Action<object> 형태일 경우
                     actionObjectListener?.Invoke(parameter);
                 }
             }
